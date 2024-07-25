e4ac6050dd6b37160963c8515d45ad7974d526af
Jan Havlik <jan.havlik@firma.seznam.cz>
2024-07-25 20:44:57 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CSV to bedgraph
+
+Super simple script to convert csv files to bedgraph with some basic params.
+
+## Usage:
+
+1. Copy `csv` files to `src/` directory.
+2. Run script as `python3 csv_to_bedgraph.py` (you need to install `pandas` first) or `poetry run csv_to_bedgraph`.
+3. Converted `bedgraph` files will be in `dst/` directory.
---
+++ csv_to_bedgraph.py
@@ -0,0 +1,41 @@
+import os
+
+try:
+    import pandas as pd
+except ImportError:
+    print("Please install the 'pandas' library.")
+
+
+def main():
+    # List to hold data from each CSV file
+    dataframes = {}
+
+    if not os.listdir('src/'):
+        print("Please copy files you want to convert into 'src/' directory.")
+        exit()
+
+    for filename in os.listdir('src/'):
+        # only csv files
+        if filename.endswith('.csv'):
+            df = pd.read_csv('src/' + filename)
+
+            # chromosome name is the filename without the extension
+            chr_name = filename.split('.')[0]
+            dataframes[chr_name] = df
+
+
+    for chr, dataframe in dataframes.items():
+        
+        with open(f"dst/{chr}.bg", "w") as bg_file:
+            bg_file.write( 
+                f"browser hide all\n" + 
+                f"browser pack refGene encodeRegions\n" +
+                f"browser full altGraph\n" + 
+                f'track type=bedGraph name="{chr} palindrome finder" description="BedGraph format of palindrome finder results" visibility=full color=200,100,0 altColor=0,100,200 priority=20\n'
+            )
+            for row in dataframe.iterrows():
+                row = row[1]
+                end = int(row["Position"]) + int(row["Spacer length"]) + int(row["Length"]) * 2
+                bg_file.write(f"{chr} {int(row['Position'])} {end} {row['Length']} {row['Spacer length']} {row['Mismatch count']}\n")
+
+
